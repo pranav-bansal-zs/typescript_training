a2834284fcf5f9f3aaabc23303b443c3f17c09ce
Pranav <raramuri@raramuri-MacBook-Pro.local>
2025-01-31 15:19:41 +0530

    Added Page buttons dynamically

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,7 @@ function App() {
   }, []);
 
   const pagedata = data.slice((page - 1) * 10, page * 10);
+  const datalength=data.length/10;
 
   return (
     <div className="container">
@@ -63,22 +64,22 @@ function App() {
           </button>
         ))}
 
-        {page < 10 && (<p className='dot'>. .</p>)}
+        {page < datalength && (<p className='dot'>. .</p>)}
 
          <button 
-         disabled={page>9}
+         disabled={page>datalength-1}
          onClick={() => {
-          setPage(prev => Math.min(10, prev + 1));
-          setButtonarr(prev => (prev[prev.length - 1] === 10 ? prev : prev.map(value => value + 1)));
+          setPage(prev => Math.min(datalength, prev + 1));
+          setButtonarr(prev => (prev[prev.length - 1] === datalength ? prev : prev.map(value => value + 1)));
           setClickedbutton((prev)=>prev+1);
         }}>{'>'}</button>
 
         <button 
-          disabled={page>9}
+          disabled={page>datalength-1}
           onClick={() => {
-          setPage(10);
-          setClickedbutton(10);
-          setButtonarr([6,7,8,9,10]);
+          setPage(datalength);
+          setClickedbutton(datalength);
+          setButtonarr([datalength-4,datalength-3,datalength-2,datalength-1,datalength]);
         }}>{'>>'}</button>
       </div>
     </div>
