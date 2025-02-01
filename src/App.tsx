aa2b2b9bfb37c834df19562cb17c509930886094
Pranav <raramuri@raramuri-MacBook-Pro.local>
2025-02-01 20:00:33 +0530

    Added the page buttons dynamically using button array

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,7 @@ interface Product {
   images: string[];
 }
 
-function App() {
-  const [buttonarr, setButtonarr] = useState<number[]>([1, 2, 3, 4, 5]);
+function App() { 
   const [clickedbutton, setClickedbutton] = useState<number>(1);
   const [data, setData] = useState<Product[]>([]);
   const [page, setPage] = useState<number>(1);
@@ -17,19 +16,22 @@ function App() {
   useEffect(() => {
     const dataFetch = async () => {
       try {
-        const res = await fetch('https://dummyjson.com/products?limit=100');
+        const res = await fetch('https://dummyjson.com/products?limit=300');
         const res1 = await res.json();
         setData(res1.products);
+        setButtonarr(Array.from({ length:Math.ceil(res1.products.length/10) > 5 ? 5 : Math.ceil(res1.products.length/10)}, (value, index) => index + 1))
       } catch (err) {
         console.error("Error->", err);
       }
     };
     dataFetch();
   }, []);
-
   const pagedata = data.slice((page - 1) * 10, page * 10);
-  const datalength=data.length/10;
-
+  const datalength=Math.ceil(data.length/10);
+  const [buttonarr, setButtonarr] = useState<number[]>([]); 
+  useEffect(()=>{
+    
+  },[datalength])
   return (
     <div className="container">
       <div className='main-container'>
@@ -43,7 +45,7 @@ function App() {
           onClick={() => {
           setPage(1);
           setClickedbutton(1);
-          setButtonarr([1,2,3,4,5]);
+          setButtonarr(Array.from({length:datalength>5?5:datalength},(value,index)=>index+1));
         }}>{'<<'}</button>
         <button 
           disabled={page<2}
@@ -69,7 +71,7 @@ function App() {
          <button 
          disabled={page>datalength-1}
          onClick={() => {
-          setPage(prev => Math.min(datalength, prev + 1));
+          setPage(prev => prev==datalength? 0:prev + 1);
           setButtonarr(prev => (prev[prev.length - 1] === datalength ? prev : prev.map(value => value + 1)));
           setClickedbutton((prev)=>prev+1);
         }}>{'>'}</button>
@@ -79,7 +81,7 @@ function App() {
           onClick={() => {
           setPage(datalength);
           setClickedbutton(datalength);
-          setButtonarr([datalength-4,datalength-3,datalength-2,datalength-1,datalength]);
+          setButtonarr(Array.from({length:datalength<5?datalength:5},(value,index)=>datalength-index).reverse());
         }}>{'>>'}</button>
       </div>
     </div>
