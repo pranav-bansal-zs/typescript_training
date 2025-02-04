ec2b2218da11c8fe4e989df3ef8aeefbabbbbbc2
Pranav <raramuri@raramuri-MacBook-Pro.local>
2025-02-04 17:01:07 +0530

    Added input container

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,25 @@
 import { useState, ChangeEvent } from 'react';
 import './App.css';
-import Carousel from './components/Carousel';
+
 
 const App = () => {
   const [inp, setInp] = useState('');
-  let timer: ReturnType<typeof setTimeout>;
 
-  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      setInp(e.target.value);
-    }, 3000);
+  const handleInput = () => {
+    let timer: ReturnType<typeof setTimeout>;
+    return(e)=>{
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        setInp(prev=>prev+e.target.value);
+      }, 3000);}
   };
-
-  // Sample images for the carousel
-  const images = [
-    'https://picsum.photos/600/400?random=1',
-    'https://picsum.photos/600/400?random=2',
-    'https://picsum.photos/600/400?random=3',
-    'https://picsum.photos/600/400?random=4',
-  ];
 
   return (
     <div className="main-input-container">
       <h1 className='headin'>Input container</h1>
       <div className='input-container'>
-        <input className='input-box' placeholder='Enter input here' onChange={handleInput} />
+        <input className='input-box' placeholder='Enter input here' onChange={handleInput()} />
         <input className='input-box' placeholder='' value={inp} readOnly />
-      </div>
-      
-      <div className="carousel-wrapper">
-        <Carousel images={images} />
       </div>
     </div>
   );
