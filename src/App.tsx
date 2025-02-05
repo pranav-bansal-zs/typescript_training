6285e68de3ae4720d552c4e72ab2f9581276e002
Pranav <raramuri@raramuri-MacBook-Pro.local>
2025-02-05 14:09:40 +0530

    Developed traffic lights

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
-import { useState, ChangeEvent } from 'react';
-import './App.css';
-
-
+import React, { useState } from 'react'
+import './App.css'
 const App = () => {
-  const [inp, setInp] = useState('');
-
-  const handleInput = () => {
-    let timer: ReturnType<typeof setTimeout>;
-    return(e)=>{
-      clearTimeout(timer);
-      timer = setTimeout(() => {
-        setInp(prev=>prev+e.target.value);
-      }, 3000);}
-  };
-
+  const [light,setLight]=useState<number>(0);
+  const obj:number[]=[5000,3000,2000];
+  setTimeout(() => {
+    setLight(prev=>(prev+1)%3);
+  }, obj[light]);
   return (
-    <div className="main-input-container">
-      <h1 className='headin'>Input container</h1>
-      <div className='input-container'>
-        <input className='input-box' placeholder='Enter input here' onChange={handleInput()} />
-        <input className='input-box' placeholder='' value={inp} readOnly />
-      </div>
+    <div className='traffic-container'>
+<div className='traffic_light-container'>
+      <div className={light==0?"red-light red-background":"red-light"} ></div>
+      <div className={light==1?"yellow-light yellow-background":"yellow-light"} ></div>
+      <div className={light==2?"green-light green-background":"green-light"} ></div>
     </div>
-  );
-};
+    <div className='small-container'>
+    </div>
+    </div>
+  )
+}
 
-export default App;
+export default App
+
+
+
